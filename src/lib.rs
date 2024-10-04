0098cff2511029f70c94da21479d2ecd7876aa91
Tomasz Drwięga <tomusdrw@gmail.com>
2024-10-04 14:16:48 +0200

    Some more idiomatic code.

--- src/lib.rs
+++ src/lib.rs
@@ -28,12 +28,14 @@ impl Shard {
     }
 }
 
+type RawShard = Vec<u8>;
+
 fn rs_encode(
     original_count: usize,
     recovery_count: usize,
     shard_bytes: usize,
-    shards: Vec<js_sys::Uint8Array>,
-) -> Result<Vec<js_sys::Uint8Array>, Error> {
+    shards: &[js_sys::Uint8Array],
+) -> Result<Vec<RawShard>, Error> {
     let mut encoder = ReedSolomonEncoder::new(original_count, recovery_count, shard_bytes)?;
 
     for shard in shards {
@@ -42,26 +44,24 @@ fn rs_encode(
 
     let result = encoder.encode()?;
 
-    return result
+    Ok(result
         .recovery_iter()
-        .map(|slice| {
-            let array = js_sys::Uint8Array::new_with_length(slice.len() as u32);
-            array.copy_from(slice);
-            Ok(array)
-        })
-        .collect();
+        .map(|v| v.to_vec())
+        .collect())
 }
 
 fn rs_decode(
     original_count: usize,
     recovery_count: usize,
     shard_bytes: usize,
-    shards: Vec<Shard>,
+    // TODO [ToDr] this might be better of as one single vector,
+    // because now we need to copy a lot from JS to Rust
+    shards: &[Shard],
 ) -> Result<Vec<Shard>, Error> {
     let mut decoder = ReedSolomonDecoder::new(original_count, recovery_count, shard_bytes)?;
 
     for shard in shards {
-        let idx_usize = usize::from(shard.index);
+        let idx_usize = shard.index as usize;
         if idx_usize < original_count {
             decoder.add_original_shard(idx_usize, shard.data.to_vec())?;
         } else {
@@ -71,37 +71,43 @@ fn rs_decode(
 
     let decoding_result = decoder.decode()?;
 
-    return decoding_result
+    Ok(decoding_result
         .restored_original_iter()
-        .map(|(idx, slice)| {
-            let array = js_sys::Uint8Array::new_with_length(slice.len() as u32);
-            array.copy_from(slice);
-            let shard = Shard::new(idx as u16, array);
-            Ok(shard)
-        })
-        .collect();
+        .map(|(idx, slice)| Shard::new(idx as u16, slice.into()))
+        .collect()
+    )
 }
 
 #[wasm_bindgen]
 pub fn encode(
-    original_count: usize,
-    recovery_count: usize,
-    shard_bytes: usize,
+    original_count: u16,
+    recovery_count: u16,
+    shard_bytes: u16,
     shards: Vec<js_sys::Uint8Array>,
-) -> Result<Vec<js_sys::Uint8Array>, String> {
-    let result = rs_encode(original_count, recovery_count, shard_bytes, shards);
+) -> Result<Vec<Shard>, String> {
+    let shards = rs_encode(
+        original_count as usize, 
+        recovery_count as usize, 
+        shard_bytes as usize, 
+        &shards
+    ).map_err(|e| e.to_string())?;
 
-    result.map_err(|e| e.to_string())
+    Ok(shards.into_iter().enumerate().map(|(idx, v)| Shard::new(idx as u16, v.as_slice().into())).collect())
 }
 
 #[wasm_bindgen]
 pub fn decode(
-    original_count: usize,
-    recovery_count: usize,
-    shard_bytes: usize,
+    original_count: u16,
+    recovery_count: u16,
+    shard_bytes: u16,
     shards: Vec<Shard>,
 ) -> Result<Vec<Shard>, String> {
-    let result = rs_decode(original_count, recovery_count, shard_bytes, shards);
+    let result = rs_decode(
+        original_count as usize,
+        recovery_count as usize,
+        shard_bytes as usize,
+        &shards
+    );
 
     result.map_err(|e| e.to_string())
 }
